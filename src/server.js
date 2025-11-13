713799c8f50ae73a4286e42b33c652eb576aa968
pchloe02 <pelerinchloe@gmail.com>
2025-11-13 14:01:08 +0100

    [Module-01] Configuration initiale du projet

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {res.json({
+    message: 'Welcome to the API', 
+    status: 'success', 
+    version: '1.0.0'});
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
