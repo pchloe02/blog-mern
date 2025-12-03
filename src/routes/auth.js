40a8afe756bc0889a57f3ae8748f2d97fe4163dd
pchloe02 <pelerinchloe@gmail.com>
2025-12-03 23:44:57 +0100

    [Auth] logout controller with invalidate token

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { register, login } from '../controllers/authController.js';
+import { register, login, logout } from '../controllers/authController.js';
+import { verifyToken } from '../middleware/auth.js';
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', verifyToken, logout);
 
 export default router;
