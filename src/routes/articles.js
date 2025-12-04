d4d13a1f754f4647be9920b2ccfc1c0aec83d92f
pchloe02 <pelerinchloe@gmail.com>
2025-12-04 11:13:14 +0100

    [Auth] fix middleware

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -10,15 +10,16 @@ import {
     getPublishedArticles,
     publishArticle
 } from '../controllers/articleController.js';
-import { verifyToken } from '../middleware/auth.js';
+import { protect } from '../middleware/auth.js';
 
 router.get('/published', getPublishedArticles);
 router.get('/', getAllArticles);
 router.get('/:id', getArticleById);
 
-router.post('/', verifyToken, createArticle);
-router.put('/:id', verifyToken, updateArticle);
-router.delete('/:id', verifyToken, deleteArticle);
-router.patch('/:id/publish', verifyToken, publishArticle);
+router.use(protect);
+router.post('/', createArticle);
+router.put('/:id', updateArticle);
+router.delete('/:id', deleteArticle);
+router.patch('/:id/publish', publishArticle);
 
 export default router;
