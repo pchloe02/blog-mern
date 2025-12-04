4453895b9fadcebdadcf4843819153e2d7eb2432
pchloe02 <pelerinchloe@gmail.com>
2025-12-04 15:13:18 +0100

    [User] controllers & routes for handling user profile info

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { register, login, logout } from '../controllers/authController.js';
+import { getMe, updateMe, updatePassword } from '../controllers/userController.js';
 import { protect } from '../middleware/auth.js';
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', protect, logout);
+
+router.use(protect);
+router.post('/logout', logout);
+router.get('/me', getMe);
+router.patch('/update-me', updateMe);
+router.patch('/update-password', updatePassword);
 
 export default router;
