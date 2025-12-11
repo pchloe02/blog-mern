35b5a6d1c2615ba0e32fc7a4924f0a915e6cb0d9
pchloe02 <pelerinchloe@gmail.com>
2025-12-11 10:38:12 +0100

    reorganize folders & setup front project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to the MERN blog !</h1>
+      <p>All the repices you need are here :) </p>
+    </>
+  );
+};
+
+export default App;
